3c85a509222f3da766844fcffaa4640fc929feba
Eliriah <46762814+Eliriah@users.noreply.github.com>
2019-02-20 16:11:55 -0700

    Add files via upload

--- Player.java
+++ Player.java
@@ -6,12 +6,14 @@ public class Player{
 	private int y_coordinate;
 	private int width;
 	private int height;
+	private boolean live;
 
 	public Player(int x,int y){
 		this.x_coordinate = x;
 		this.y_coordinate = y;
 		this.width = 5;
 		this.height = 5;
+		this.live = true;
 	}
 	public int getX_Coordinate(){
 		return x_coordinate;
@@ -19,11 +21,17 @@ public class Player{
 	public int getY_Coordinate(){
 		return y_coordinate;
 	}
+	public boolean getLive(){
+		return live;
+	}
 	public void moveRight(){
 		x_coordinate += 5;
 	}
 	public void moveleft(){
 		x_coordinate -= 5;
+	}
+	public void setLive(boolean state){
+		live = state;
 	}
 	public Rectangle getPlayerBoundry(){
 		return new Rectangle(x_coordinate, y_coordinate, width, height);
