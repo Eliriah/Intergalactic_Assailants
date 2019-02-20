689204dbdc9b5956296c7b5e257a88ed61788ccd
rach-nguyen <45080595+rach-nguyen@users.noreply.github.com>
2019-02-20 16:10:42 -0700

    Add files via upload

--- AnimationApp.java
+++ AnimationApp.java
@@ -1,43 +1,25 @@
-import java.util.scanner;
+import java.util.Scanner;
 public class AnimationApp{
-    private Player player;
-    private ArrayList<Obstacle> obstacles;
-
-    /*public void printCurrentState(){
-        
-
-
-    }*/
-
     public static void main(String[] args) {
         System.out.println("Press the 'a' key to move left. Press the 'd' key to move right. Press the spacebar to shoot.");
         Scanner keyboard = new Scanner(System.in);
-        String keyEntered = keyboard.nextLine();
-        if (keyEntered == 'a'){
-            if(overlapsWith){
-                System.out.println("Sorry, you can't move any farther left.");
-            }
-            else{
-            //move avatar's x coordinate -1 
-            System.out.println(/*new coordinate*/);
-            }
-
-        }
-        if(keyEntered == 'd'){
-            if(overlapsWith){
-                System.out.println("Sorry, you can't move any farther right.");
-            }
-            else{
-            //move avatar's x coordinate +1
-            System.out.println(/*new coordinate*/);
-            }
-
-
-        }
-        if (keyEntered == ' '){
-            //shoot projectile up (y coordi +1) until it hits bound or collides with an enemy.
-            System.out.println(/*new coordinates*/);
-        }
+        char keyEntered = keyboard.next().charAt(0);
+        Player player1 = new Player(0,0);
         
+        if (keyEntered == 'a'){
+            /*if(overlapsWith){
+                System.out.println("Sorry, you can't move any farther left.");
+            }*/
+            player1.moveLeft();
+            System.out.println(player1.getX_Coordinate());
+        }
+
+        if(keyEntered == 'd'){
+            /*if(overlapsWith){
+                System.out.println("Sorry, you can't move any farther right.");
+            }*/
+            player1.moveRight();
+            System.out.println(player1.getX_Coordinate());
+        }
     }
 }
