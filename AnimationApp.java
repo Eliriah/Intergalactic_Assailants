c2d42525db9eb8b0bbe3665945af52462e59a24f
rach-nguyen <45080595+rach-nguyen@users.noreply.github.com>
2019-02-20 16:17:48 -0700

    Add files via upload

--- AnimationApp.java
+++ AnimationApp.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 public class AnimationApp{
     public static void main(String[] args) {
         System.out.println("Press the 'a' key to move left. Press the 'd' key to move right. Press the spacebar to shoot.");
-        Scanner keyboard = new Scanner(System.in);
-        char keyEntered = keyboard.next().charAt(0);
         Player player1 = new Player(0,0);
-        
-        if (keyEntered == 'a'){
-            /*if(overlapsWith){
-                System.out.println("Sorry, you can't move any farther left.");
-            }*/
-            player1.moveLeft();
-            System.out.println(player1.getX_Coordinate());
+        while(player1.getLive() == true){
+            Scanner keyboard = new Scanner(System.in);
+            char keyEntered = keyboard.next().charAt(0);
+            if (keyEntered == 'a'){
+                if(player1.getX_Coordinate() == (-50)){
+                    System.out.println("Sorry, you can't move any farther left.");
+                }
+                else{
+                    player1.moveLeft();
+                    System.out.println(player1.getX_Coordinate());
+                }
+
+            }
+    
+            if(keyEntered == 'd'){
+                if(player1.getX_Coordinate() == 50){
+                    System.out.println("Sorry, you can't move any farther right.");
+                }
+                else{
+                    player1.moveRight();
+                    System.out.println(player1.getX_Coordinate());
+                }
+
+            }
+
         }
 
-        if(keyEntered == 'd'){
-            /*if(overlapsWith){
-                System.out.println("Sorry, you can't move any farther right.");
-            }*/
-            player1.moveRight();
-            System.out.println(player1.getX_Coordinate());
-        }
     }
 }
