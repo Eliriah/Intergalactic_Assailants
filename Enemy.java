2ae00f70e334ba5078a16075cd639516ac634527
rach-nguyen <45080595+rach-nguyen@users.noreply.github.com>
2019-02-20 16:25:15 -0700

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,17 @@
+public class Enemy{
+    
+    private int x_coordinate;
+    private int y_coordinate;
+    private int width;
+    private int height;
+    private boolean live;
+
+    public Enemy(int x, int y){
+        this.x_coordinate = x;
+        this.y_coordinate = y;
+        this.width = 5;
+        this.height = 5;
+        this.live = true;
+    }
+    public 
+}
